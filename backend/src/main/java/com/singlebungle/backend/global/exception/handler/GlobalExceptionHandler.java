71af9f65289d4d570d153f37cd64b9d77e02db00
dayullee <dy007lukyday@gmail.com>
2024-10-25 15:49:02 +0900

    fix: 컴파일 오류 수정

--- backend/src/main/java/com/singlebungle/backend/global/exception/handler/GlobalExceptionHandler.java
+++ backend/src/main/java/com/singlebungle/backend/global/exception/handler/GlobalExceptionHandler.java
@@ -1,7 +1,7 @@
 package com.singlebungle.backend.global.exception.handler;
 
-import com.newzy.backend.global.exception.*;
-import com.newzy.backend.global.exception.model.ExceptionResponseDto;
+import com.singlebungle.backend.global.exception.model.ExceptionResponseDto;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -26,33 +26,6 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler(CustomIllegalStateException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<ExceptionResponseDto> handleCustomIllegalStateException(CustomIllegalStateException ex, HttpServletRequest request) {
-        log.error("CustomIllegalStateException 발생 - URL: {}, Message: {}", request.getRequestURI(), ex.getMessage());
-
-        ExceptionResponseDto response = ExceptionResponseDto.of(
-                request.getMethod(),
-                request.getRequestURI(),
-                HttpStatus.BAD_REQUEST.value(),
-                ex.getMessage()
-        );
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-    }
-
-    @ExceptionHandler(NotValidRequestException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<ExceptionResponseDto> handleNotValidRequestException(NotValidRequestException ex, HttpServletRequest request) {
-        log.error("NotValidRequestException 발생 - URL: {}, Message: {}", request.getRequestURI(), ex.getMessage());
-
-        ExceptionResponseDto response = ExceptionResponseDto.of(
-                request.getMethod(),
-                request.getRequestURI(),
-                HttpStatus.BAD_REQUEST.value(),
-                ex.getMessage()
-        );
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
-    }
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponseDto> handleUnknownException(
@@ -67,32 +40,4 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-
-    @ExceptionHandler(EntityIsFoundException.class)
-    public ResponseEntity<ExceptionResponseDto> handleEntityIsFoundException(
-            Exception ex, HttpServletRequest request) {
-        log.error("EntityIsFoundException 발생 - URL: {}, Message: {}", request.getRequestURI(), ex.getMessage());
-        ExceptionResponseDto response = ExceptionResponseDto.of(
-                request.getMethod(),
-                request.getRequestURI(),
-                HttpStatus.CONFLICT.value(),
-                ex.getMessage()
-        );
-        return new ResponseEntity<>(response, HttpStatus.CONFLICT);
-    }
-
-    @ExceptionHandler(StringLengthLimitException.class)
-    public ResponseEntity<ExceptionResponseDto> handleStringLengthLimitException(
-            StringLengthLimitException ex, HttpServletRequest request
-    ) {
-        log.error("StringLengthLimitException 발생 - URL: {}, Message: {}", request.getRequestURI(), ex.getMessage());
-        ExceptionResponseDto response = ExceptionResponseDto.of(
-                request.getMethod(),
-                request.getRequestURI(),
-                HttpStatus.BAD_REQUEST.value(),
-                ex.getMessage()
-        );
-
-        return new ResponseEntity<>(response, HttpStatus.CONFLICT);
-    }
 }
