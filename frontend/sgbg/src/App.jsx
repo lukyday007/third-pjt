8c3f1c9c2cc71c82ae0b65e3e184c894f6c741fe
김재훈 <kgarry@naver.com>
2024-10-25 14:12:15 +0900

    init: 프론트엔드 프로젝트 생성

---
+++ frontend/sgbg/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>싱글벙글</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/sgbg/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+
+// If you want use Node.js, the`nodeIntegration` needs to be enabled in the Main process.
+// import './demos/node'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+
+postMessage({ payload: 'removeLoading' }, '*')
