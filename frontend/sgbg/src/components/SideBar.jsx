a6b70722b2b2569aba022011d80cfe82259bda36
김고은 <kke0402@naver.com>
2024-10-28 14:06:13 +0900

    feat: ToggleSideBar

--- frontend/sgbg/src/components/SideBar.jsx
+++ frontend/sgbg/src/components/SideBar.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
 import HomeIcon from "../asset/images/SideBar/HomeIcon.svg?react"
 import SideBarToggleIcon from "../asset/images/SideBar/SideBarToggleIcon.svg?react"
@@ -14,11 +14,11 @@ import TestImage from "../asset/images/TestImage.svg?react"
 
 const s = {
   Container: styled.div`
-    border: solid 1px #e1e3e1;
+    border-right: solid 1px #e1e3e1;
     width: 300px;
     padding: 28px;
-    display: flex;
     flex-direction: column;
+    display: ${(props) => (props.$isopen === true ? "flex" : "none")};
   `,
   HomeIconArea: styled.div`
     display: flex;
@@ -48,48 +48,70 @@ const s = {
     display: flex;
     align-items: center;
     margin-top: 8px;
+    cursor: pointer;
+  `,
+  ClosedSidebar: styled.div`
+    padding: 35px 28px 0 28px;
+    display: ${(props) => (props.$isopen === true ? "none" : "inline")};
   `,
 }
 
 const SideBar = () => {
+  const [isSideBarOpen, setIsSideBarOpen] = useState(true)
+
+  const toggleSideBar = () => {
+    setIsSideBarOpen((prev) => !prev)
+  }
+
   return (
-    <s.Container>
-      <s.HomeIconArea>
-        <HomeIcon style={{ cursor: "pointer" }} />
-        <SideBarToggleIcon style={{ cursor: "pointer" }} />
-      </s.HomeIconArea>
-      <s.UserInfoArea>
-        <TestImage />
-        <s.Email>kke0402@naver.com</s.Email>
-      </s.UserInfoArea>
-      <s.FolderCaption>기본</s.FolderCaption>
-      <s.FolderArea>
-        <AllImagesIcon />
-        <s.FolderTitle>전체 이미지</s.FolderTitle>
-      </s.FolderArea>
-      <s.FolderArea>
-        <DefaultFolderIcon />
-        <s.FolderTitle>기본폴더</s.FolderTitle>
-      </s.FolderArea>
-      <s.FolderCaption>내 폴더</s.FolderCaption>
-      <s.FolderArea>
-        <CommonFolderIcon />
-        <s.FolderTitle>싱글벙글한 이미지</s.FolderTitle>
-      </s.FolderArea>
-      <s.FolderCaption>관리</s.FolderCaption>
-      <s.FolderArea>
-        <CreateFolderIcon />
-        <s.FolderTitle>폴더 만들기</s.FolderTitle>
-      </s.FolderArea>
-      <s.FolderArea style={{ marginBottom: "32px" }}>
-        <TrashBinIcon />
-        <s.FolderTitle>휴지통</s.FolderTitle>
-      </s.FolderArea>
-      <s.FolderArea style={{ marginTop: "auto" }}>
-        <SettingsIcon />
-        <s.FolderTitle>설정</s.FolderTitle>
-      </s.FolderArea>
-    </s.Container>
+    <>
+      <s.Container $isopen={isSideBarOpen}>
+        <s.HomeIconArea>
+          <HomeIcon style={{ cursor: "pointer" }} />
+          <SideBarToggleIcon
+            onClick={toggleSideBar}
+            style={{ cursor: "pointer" }}
+          />
+        </s.HomeIconArea>
+        <s.UserInfoArea>
+          <TestImage />
+          <s.Email>kke0402@naver.com</s.Email>
+        </s.UserInfoArea>
+        <s.FolderCaption>기본</s.FolderCaption>
+        <s.FolderArea>
+          <AllImagesIcon />
+          <s.FolderTitle>전체 이미지</s.FolderTitle>
+        </s.FolderArea>
+        <s.FolderArea>
+          <DefaultFolderIcon />
+          <s.FolderTitle>기본폴더</s.FolderTitle>
+        </s.FolderArea>
+        <s.FolderCaption>내 폴더</s.FolderCaption>
+        <s.FolderArea>
+          <CommonFolderIcon />
+          <s.FolderTitle>싱글벙글한 이미지</s.FolderTitle>
+        </s.FolderArea>
+        <s.FolderCaption>관리</s.FolderCaption>
+        <s.FolderArea>
+          <CreateFolderIcon />
+          <s.FolderTitle>폴더 만들기</s.FolderTitle>
+        </s.FolderArea>
+        <s.FolderArea>
+          <TrashBinIcon />
+          <s.FolderTitle>휴지통</s.FolderTitle>
+        </s.FolderArea>
+        <s.FolderArea style={{ marginTop: "auto" }}>
+          <SettingsIcon />
+          <s.FolderTitle>설정</s.FolderTitle>
+        </s.FolderArea>
+      </s.Container>
+      <s.ClosedSidebar $isopen={isSideBarOpen}>
+        <SideBarToggleIcon
+          onClick={toggleSideBar}
+          style={{ cursor: "pointer" }}
+        />
+      </s.ClosedSidebar>
+    </>
   )
 }
 
