42b9a2e5f8f99b090d2eb822f2f5666cf40a80f3
dayullee <dy007lukyday@gmail.com>
2024-11-05 00:14:03 +0900

    feat: elasticsearch 기능 추가

--- backend/src/main/java/com/singlebungle/backend/global/config/ElasticSearchConfig.java
+++ backend/src/main/java/com/singlebungle/backend/global/config/ElasticSearchConfig.java
@@ -1,22 +1,38 @@
 package com.singlebungle.backend.global.config;
 
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
 import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
-@EnableElasticsearchRepositories(basePackages = "org.springframework.data.elasticsearch.repository")
+@EnableElasticsearchRepositories(basePackages = "com.singlebungle.backend.domain.search.repository")
 public class ElasticSearchConfig {
 
     @Bean
     public RestHighLevelClient client() {
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("<username>", "<password>"));
+
         return new RestHighLevelClient(
                 RestClient.builder(
                         new HttpHost("k11b205.p.ssafy.io", 9200, "http")
                 )
+                .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+                    @Override
+                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+                        return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                    }
+                })
         );
     }
 }
