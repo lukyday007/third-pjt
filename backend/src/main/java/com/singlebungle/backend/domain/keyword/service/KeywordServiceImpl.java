d4143cd81fd2d44bd4c91826fdcdf77ac542301c
dayullee <dy007lukyday@gmail.com>
2024-11-08 14:13:41 +0900

    update : redis 직렬화

--- backend/src/main/java/com/singlebungle/backend/domain/keyword/service/KeywordServiceImpl.java
+++ backend/src/main/java/com/singlebungle/backend/domain/keyword/service/KeywordServiceImpl.java
@@ -25,37 +25,36 @@ public class KeywordServiceImpl implements KeywordService {
     @Override
     @Transactional
     public void saveKeyword(List<String> keywords) {
-
         /*
             키워드가 새로 저장되면 일단 redisKeyword에 저장
             중복된 키워드가 있으면 redisKeyword의 value + 1
             레디스에 다시 저장
         */
-
         for (String name : keywords) {
             boolean isKeyword = keywordRepository.existsByKeywordName(name);
 
             if (isKeyword) {
                 // Redis에서 현재 curCnt 값 가져오기
-                String curCntStr = (String) keywordTemplate.opsForHash().get("keyword:" + name, "curCnt");
+                String curCntStr = (String) keywordTemplate.opsForHash().get("keyword", name + ":curCnt");
                 if (curCntStr == null) {
                     // curCnt가 없는 경우 초기값 설정
-                    keywordTemplate.opsForHash().put("keyword:" + name, "curCnt", "1");
+                    log.warn(">>> redis >>> curCntStr 이 null 일 때 curCnt, prevCnt 값 셋팅");
+                    keywordTemplate.opsForHash().put("keyword", name + ":curCnt", "1");
+                    keywordTemplate.opsForHash().put("keyword", name + ":prevCnt", "1");
                 } else {
                     int curCnt = Integer.parseInt(curCntStr) + 1;
-                    keywordTemplate.opsForHash().put("keyword:" + name, "curCnt", String.valueOf(curCnt));
+                    keywordTemplate.opsForHash().put("keyword", name + ":curCnt", String.valueOf(curCnt));
                 }
-
                 continue;
             }
 
-            // 키워드 저장
+            // 키워드 새로 저장
             Keyword kw = Keyword.convertToEntity(name);
             keywordRepository.save(kw);
-            // Redis 데이터 초기 설정
-            keywordTemplate.opsForHash().put("keyword:" + name, "prevCnt", "1");
-            keywordTemplate.opsForHash().put("keyword:" + name, "curCnt", "1");
 
+            // Redis 데이터 초기 설정 (하나의 해시 키 "keyword"에 저장)
+            keywordTemplate.opsForHash().put("keyword", name + ":prevCnt", "1");
+            keywordTemplate.opsForHash().put("keyword", name + ":curCnt", "1");
         }
     }
 }
--- backend/src/main/java/com/singlebungle/backend/global/config/RedisConfig.java
+++ backend/src/main/java/com/singlebungle/backend/global/config/RedisConfig.java
@@ -33,62 +33,66 @@ public class RedisConfig {
     @Value("${spring.redis-user.port}")
     private int redisUserPort;
 
-    // 기본 Redis 인스턴스 설정
+    /**
+     * 기본 Redis 인스턴스 설정
+     */
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        LettuceConnectionFactory factory = new LettuceConnectionFactory(redisHost, redisPort);
-        factory.setPassword(redisPassword);
-        return factory;
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+        config.setPassword(redisPassword);
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(new StringRedisSerializer());
-        template.setConnectionFactory(redisConnectionFactory());
-
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
     }
 
-
-    // redis-keyword 인스턴스 설정
+    /**
+     * Redis-Keyword 인스턴스 설정
+     */
     @Bean
     public RedisConnectionFactory redisKeywordConnectionFactory() {
-        LettuceConnectionFactory factory = new LettuceConnectionFactory(redisKeywordHost, redisKeywordPort);
-        factory.setPassword(redisPassword);
-        return factory;
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisKeywordHost, redisKeywordPort);
+        config.setPassword(redisPassword);
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean(name = "redisKeywordTemplate")
-    public RedisTemplate<String, Object> redisKeywordTemplate() {
+    public RedisTemplate<String, Object> redisKeywordTemplate(RedisConnectionFactory redisKeywordConnectionFactory) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisKeywordConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(new StringRedisSerializer());
-        template.setConnectionFactory(redisKeywordConnectionFactory());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
     }
 
-
-    // redis-user 인스턴스 설정
+    /**
+     * Redis-User 인스턴스 설정
+     */
     @Bean
     public RedisConnectionFactory redisUserConnectionFactory() {
-        LettuceConnectionFactory factory = new LettuceConnectionFactory(redisUserHost, redisUserPort);
-        factory.setPassword(redisPassword);
-        return factory;
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisUserHost, redisUserPort);
+        config.setPassword(redisPassword);
+        return new LettuceConnectionFactory(config);
     }
 
     @Bean(name = "redisUserTemplate")
-    public RedisTemplate<String, String> redisUserTemplate() {
-        RedisTemplate<String, String> template = new RedisTemplate<>();
+    public RedisTemplate<String, Object> redisUserTemplate(RedisConnectionFactory redisUserConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisUserConnectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
-        template.setConnectionFactory(redisUserConnectionFactory());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
         return template;
     }
-
 }
