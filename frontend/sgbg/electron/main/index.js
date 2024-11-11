3298a95c9b00cc504aaa588951fed6a3943a1624
조규범 <standardjo@naver.com>
2024-11-11 17:41:19 +0900

    auto update8

--- frontend/sgbg/electron/main/index.js
+++ frontend/sgbg/electron/main/index.js
@@ -24,6 +24,7 @@ const VITE_DEV_SERVER_URL = process.env.VITE_DEV_SERVER_URL
 
 let win = BrowserWindow
 let progressBar = null
+let autoUpdater = null
 const preload = path.join(__dirname, "../preload/index.mjs")
 const indexHtml = path.join(RENDERER_DIST, "index.html")
 
@@ -39,57 +40,65 @@ async function createWindow() {
   win.loadFile(indexHtml)
 }
 
-// autoUpdater 및 error event 설정
-autoUpdater.autoDownload = false
-autoUpdater.autoInstallOnAppQuit = false
+// `import()`로 electron-updater 동적 로딩
+async function initAutoUpdater() {
+  const updaterModule = await import("electron-updater")
+  autoUpdater = updaterModule.autoUpdater
 
-app.whenReady().then(() => {
-  createWindow()
+  // autoUpdater 설정
+  autoUpdater.autoDownload = false
+  autoUpdater.autoInstallOnAppQuit = false
+
   autoUpdater.checkForUpdates()
-})
 
-autoUpdater.on("update-available", () => {
-  dialog
-    .showMessageBox({
-      type: "info",
-      title: "Update",
-      message: "새로운 버전이 있습니다. 다운로드하시겠습니까?",
-      buttons: ["예", "아니오"],
-    })
-    .then((result) => {
-      if (result.response === 0) autoUpdater.downloadUpdate()
-    })
-})
+  autoUpdater.on("update-available", () => {
+    dialog
+      .showMessageBox({
+        type: "info",
+        title: "Update",
+        message: "새로운 버전이 있습니다. 다운로드하시겠습니까?",
+        buttons: ["예", "아니오"],
+      })
+      .then((result) => {
+        if (result.response === 0) autoUpdater.downloadUpdate()
+      })
+  })
 
-autoUpdater.on("download-progress", (progressObj) => {
-  if (!progressBar) {
-    progressBar = new ProgressBar({
-      text: "다운로드 중...",
-      detail: "파일을 다운로드 중입니다.",
-    })
-  }
-  progressBar.value = progressObj.percent
-})
+  autoUpdater.on("download-progress", (progressObj) => {
+    if (!progressBar) {
+      progressBar = new ProgressBar({
+        text: "다운로드 중...",
+        detail: "파일을 다운로드 중입니다.",
+      })
+    }
+    progressBar.value = progressObj.percent
+  })
 
-autoUpdater.on("update-downloaded", () => {
-  if (progressBar) {
-    progressBar.setCompleted()
-    progressBar = null
-  }
-  dialog
-    .showMessageBox({
-      type: "info",
-      title: "업데이트 완료",
-      message: "새 버전이 다운로드되었습니다. 지금 설치하시겠습니까?",
-      buttons: ["예", "아니오"],
-    })
-    .then((result) => {
-      if (result.response === 0) {
-        setTimeout(() => {
-          autoUpdater.quitAndInstall()
-        }, 1000) // 1초 대기 후 quitAndInstall 호출
-      }
-    })
+  autoUpdater.on("update-downloaded", () => {
+    if (progressBar) {
+      progressBar.setCompleted()
+      progressBar = null
+    }
+    dialog
+      .showMessageBox({
+        type: "info",
+        title: "업데이트 완료",
+        message: "새 버전이 다운로드되었습니다. 지금 설치하시겠습니까?",
+        buttons: ["예", "아니오"],
+      })
+      .then((result) => {
+        if (result.response === 0) {
+          setTimeout(() => {
+            autoUpdater.quitAndInstall()
+          }, 1000) // 1초 대기 후 quitAndInstall 호출
+        }
+      })
+  })
+}
+
+app.whenReady().then(async () => {
+  createWindow()
+  await initAutoUpdater() // autoUpdater 초기화 함수 호출
 })
 
 app.on("before-quit", () => {
