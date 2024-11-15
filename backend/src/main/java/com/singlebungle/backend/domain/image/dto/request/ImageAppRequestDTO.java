06639011b6227b668eaff3859448b542a215da46
dayullee <dy007lukyday@gmail.com>
2024-11-15 10:59:18 +0900

    fix : directoryId 가 null일 때 처리

--- backend/src/main/java/com/singlebungle/backend/domain/image/dto/request/ImageAppRequestDTO.java
+++ backend/src/main/java/com/singlebungle/backend/domain/image/dto/request/ImageAppRequestDTO.java
@@ -2,15 +2,13 @@ package com.singlebungle.backend.domain.image.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @RequiredArgsConstructor
 @AllArgsConstructor
+@Builder
 @Schema(title = "IMAGE_APP_REQ : 이미지 앱 요청 DTO")
 public class ImageAppRequestDTO {
 
@@ -18,8 +16,8 @@ public class ImageAppRequestDTO {
     @Schema(description = "imageId", required = true)
     private Long imageId;
 
-    @NotNull(message = "directoryId를 입력해주세요.")
-    @Schema(description = "directoryId", required = true)
-    private Long directoryId;
+    @Schema(description = "directory id")
+    @Builder.Default
+    private Long directoryId = 0L;
 
 }
