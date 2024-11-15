c1d414039dc8e9fd762a927efaffb3eb30e91fa8
dayullee <dy007lukyday@gmail.com>
2024-11-15 13:50:00 +0900

    update: 키워드 조회수 증가

--- backend/src/main/java/com/singlebungle/backend/domain/keyword/repository/KeywordRepository.java
+++ backend/src/main/java/com/singlebungle/backend/domain/keyword/repository/KeywordRepository.java
@@ -1,5 +1,6 @@
 package com.singlebungle.backend.domain.keyword.repository;
 
+import com.singlebungle.backend.domain.image.entity.Image;
 import com.singlebungle.backend.domain.keyword.entity.Keyword;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +10,5 @@ public interface KeywordRepository extends JpaRepository<Keyword, Long> {
     boolean existsByKeywordName(String keyword);
 
     Keyword findByKeywordName(String keyword);
+
 }
